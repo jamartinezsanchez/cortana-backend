20c8a1bdc26d22fb9596a00a02ed0701d70d9585
proyectodjango2024 <proyectodjango2024@gmail.com>
2025-12-12 20:58:37 -0500

    Conexión a Supabase y creación de notas

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from supabase import create_client
+import os
 
 app = FastAPI(title="Cortana Assistant API")
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 @app.get("/")
 def home():
     return {"message": "Cortana está en línea 🤖"}
+
+@app.post("/notes")
+def create_note(title: str, content: str):
+    response = supabase.table("notes").insert({
+        "title": title,
+        "content": content
+    }).execute()
+
+    return {
+        "status": "ok",
+        "note": response.data
+    }
