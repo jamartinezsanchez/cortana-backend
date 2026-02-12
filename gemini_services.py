63ab73a678fc6018a24e412fab1343acdb80e8da
proyectodjango2024 <proyectodjango2024@gmail.com>
2026-02-11 22:37:21 -0500

    Fix Gemini SDK version

--- gemini_services.py
+++ gemini_services.py
@@ -1,18 +1,18 @@
 import os
 from google import genai
 
-client = genai.Client(
-    api_key=os.getenv("GEMINI_API_KEY")
-)
-
 def ask_gemini(message: str):
     try:
+        client = genai.Client(
+            api_key=os.getenv("GEMINI_API_KEY")
+        )
+
         response = client.models.generate_content(
-            model="gemini-1.5-flash-latest",
-            contents=message
+            model="gemini-1.5-flash",
+            contents=message,
         )
 
         return response.text
 
     except Exception as e:
-        return f"Error al comunicarse con Gemini: {str(e)}"
+        return str(e)
