a946448f943e3c48a820bce9e18474d57d18bd46
proyectodjango2024 <proyectodjango2024@gmail.com>
2026-02-11 22:49:44 -0500

    Fix Gemini model and update requirements

--- gemini_services.py
+++ gemini_services.py
@@ -1,18 +1,14 @@
-import os
 from google import genai
+import os
 
 def ask_gemini(message: str):
-    try:
-        client = genai.Client(
-            api_key=os.getenv("GEMINI_API_KEY")
-        )
+    client = genai.Client(
+        api_key=os.getenv("GEMINI_API_KEY")
+    )
 
-        response = client.models.generate_content(
-            model="gemini-1.5-flash",
-            contents=message,
-        )
+    response = client.models.generate_content(
+        model="gemini-1.5-flash",
+        contents=message
+    )
 
-        return response.text
-
-    except Exception as e:
-        return str(e)
+    return response.text
